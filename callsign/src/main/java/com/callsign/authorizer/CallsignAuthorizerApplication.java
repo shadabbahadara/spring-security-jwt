fe1f4ab37f0e8afff37dc915bdaf592b412f8111
shadab.khan@ihsmarkit.com <shadab.khan@ihsmarkit.com>
2022-01-08 14:11:37 +0530

    initial commit

---
+++ callsign/src/main/java/com/callsign/authorizer/CallsignAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package com.callsign.authorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallsignAuthorizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallsignAuthorizerApplication.class, args);
+    }
+
+}
---
+++ callsign/src/test/java/com/callsign/authorizer/CallsignAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callsign.authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallsignAuthorizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
